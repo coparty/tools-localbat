73e797b3afda6dc52d4edd5e39449523d9097643
Zeuxis Lo <seekstudio@gmail.com>
2020-05-27 13:54:01 +0800

    migrate the dotenv to the program and try to print the remote path and infomation

--- index.py
+++ index.py
@@ -1,15 +1,28 @@
 #!/usr/bin/python
 
 import os
+import sys
 from dotenv import load_dotenv
 from fabric import Connection
 
 def main():
-    print(os.getenv("SSH_HOST"))
-    print(os.getenv("SSH_PORT"))
-    print(os.getenv("SSH_USER"))
-    print(os.getenv("SSH_KEY"))
-    pass
+    ssh_mode = os.getenv("SSH_MODE", "custom")
+    ssh_host = os.getenv("SSH_HOST", None)
+    ssh_port = os.getenv("SSH_PORT", None)
+    ssh_user = os.getenv("SSH_USER", None)
+    ssh_key  = os.getenv("SSH_KEY")
+
+    c = None
+
+    if ssh_mode == "ssh_config":
+        c = Connection(ssh_host)
+    else:
+        c = Connection(ssh_host, user=ssh_user, port=ssh_port, connect_kwargs={
+            "key_filename": ssh_key
+        })
+
+    c.run("pwd")
+    c.run("uname -a")
 
 if __name__ == "__main__":
     load_dotenv()
