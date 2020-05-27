9764206abc5b1bccc288627435259a4fd439c0a7
Zeuxis Lo <seekstudio@gmail.com>
2020-05-27 20:27:18 +0800

    support backup projects into backup directory with zip by date

--- index.py
+++ index.py
@@ -1,13 +1,26 @@
 #!/usr/bin/python
 
 import json
+from os import path
+from datetime import datetime, timezone, timedelta
 from fabric import Connection
 
+def local_time() -> datetime:
+    tz = timezone(timedelta(hours=+8))
+
+    return datetime.now(tz)
+
 def main():
+    # Read the config from config file
+    print("Reading config file ...")
+
     config = json.load(open("./config.json"))
 
     ssh    = config["ssh"]
     backup = config["backup"]
+
+    # Try to connect remote server
+    print("Start connecting ...")
 
     c = None
 
@@ -18,8 +31,22 @@ def main():
             "key_filename": ssh["key"]
         })
 
-    c.run("pwd")
-    c.run("uname -a")
+    # Loop projects
+    print("Start backup projects ...")
+
+    for project_name, project_path in backup["from"].items():
+        print("=> {}".format(project_name))
+
+        to_zip_path      = backup["to"][project_name]
+        backup_directory = path.dirname(to_zip_path)
+
+        backup_date = local_time().strftime("%Y%m%d")
+        to_zip_path = to_zip_path.replace("{date}", backup_date)
+
+        c.run("mkdir -p {}".format(backup_directory))
+        c.run("tar zcPf {} {}".format(to_zip_path, project_path))
+
+    print("finished!")
 
 if __name__ == "__main__":
     main()
