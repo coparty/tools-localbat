edf865552dd716a9552bd4b0a454e763450fc14c
Zeuxis Lo <seekstudio@gmail.com>
2020-05-27 19:31:58 +0800

    change config file from dotenv to json

--- index.py
+++ index.py
@@ -1,29 +1,25 @@
 #!/usr/bin/python
 
-import os
-import sys
-from dotenv import load_dotenv
+import json
 from fabric import Connection
 
 def main():
-    ssh_mode = os.getenv("SSH_MODE", "custom")
-    ssh_host = os.getenv("SSH_HOST", None)
-    ssh_port = os.getenv("SSH_PORT", None)
-    ssh_user = os.getenv("SSH_USER", None)
-    ssh_key  = os.getenv("SSH_KEY")
+    config = json.load(open("./config.json"))
+
+    ssh    = config["ssh"]
+    backup = config["backup"]
 
     c = None
 
-    if ssh_mode == "ssh_config":
-        c = Connection(ssh_host)
+    if ssh["mode"] == "ssh_config":
+        c = Connection(ssh["host"])
     else:
-        c = Connection(ssh_host, user=ssh_user, port=ssh_port, connect_kwargs={
-            "key_filename": ssh_key
+        c = Connection(ssh["host"], user=ssh["user"], port=ssh["port"], connect_kwargs={
+            "key_filename": ssh["key"]
         })
 
     c.run("pwd")
     c.run("uname -a")
 
 if __name__ == "__main__":
-    load_dotenv()
     main()
